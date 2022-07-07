c54c3348d9f58b1e9d095775b4f2a9e5a839b458
João Vitor <joaovitor55670@gmail.com>
2022-07-07 18:55:29 -0300

    sexto commit

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-let a = document.getElementById("masc")
+let submit = document.getElementById("submit");
+let masc = document.getElementById("masc");
+let fem = document.getElementById("fem")
+let img = document.getElementsByTagName("img")[0]
+let div = document.getElementsByTagName("div")[0]
+
+console.log(div)
+
+function action() {
+
+    let nasc = parseInt(document.getElementById("numero").value);
+    let h2 = document.getElementsByTagName("h2")[0]
+    let data = new Date().getFullYear();
+    let idade = data - nasc
+    div.style = "height:430px"
+
+    if (masc.checked) {
+        h2.innerText = `Detectamos homem com ${idade} anos`
+        img.src = "./images/h.jpg"
+        
+        
+    } 
+    else if (fem.checked) {
+        h2.innerText = `Detectamos mulher com  ${idade}  anos`
+        img.src = "./images/m.jpg"
+        
+        
+    }  
+}
 
 
-console.log(a.checked)
+
+
+
+
+
+
+
+
+
+
+submit.addEventListener("click", action);
